fc8a81bdf1b9a43e9a9816504b7b29a51a718447
fjy0326 <bungakukai2010@gmail.com>
2024-09-15 05:04:21 +0000

    finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,24 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    @books = @user.books.page(params[:page])
   end
 
   def edit
     @user = User.find(params[:id])
+    unless current_user.id == @user.id
+      redirect_to user_path(current_user)
+    end
   end
   
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path(current_user.id)
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+    flash[:notice] = "You have updated user successfully."
+     redirect_to user_path(current_user.id)
+    else 
+     render :edit
+   end
   end
 
   def index
